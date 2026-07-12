c3658111714601197913f18fd6c691a94a09aaac
Raman Kakotkin <t3rixis@gmail.com>
2026-07-12 20:03:58 +0300

    feat: add elk

--- src/NotificationService.Api/Extensions/WebApplicationBuilderExtensions.cs
+++ src/NotificationService.Api/Extensions/WebApplicationBuilderExtensions.cs
@@ -1,4 +1,7 @@
 ﻿using NotificationService.Application.Abstractions.DataAccess;
+using Serilog;
+using Serilog.Events;
+using System.Reflection;
 
 namespace NotificationService.Api.Extensions
 {
@@ -9,6 +12,33 @@ namespace NotificationService.Api.Extensions
             using var scope = app.Services.CreateAsyncScope();
             var dbInitialize = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
             await dbInitialize.InitializeAsync();
+        }
+
+        public static WebApplicationBuilder AddElk(this WebApplicationBuilder app)
+        {
+            app.Host.UseSerilog((context, config) =>
+            {
+                var enviroment = context.HostingEnvironment.EnvironmentName;
+                var assembly = Assembly.GetExecutingAssembly().GetName().Name;
+
+                config.ReadFrom.Configuration(context.Configuration)
+                      .Enrich.FromLogContext()
+                      .Enrich.WithProperty("Enviroment", enviroment)
+                      .Enrich.WithProperty("Application", assembly)
+                      .Enrich.WithProperty("Service", "notification-service")
+                      .WriteTo.Console(restrictedToMinimumLevel: LogEventLevel.Information)
+                      .WriteTo.Async(a => a.File(
+                          path: "/logs/notification-service/log-.json",
+                          rollingInterval: RollingInterval.Day,
+                          retainedFileCountLimit: 7,
+                          fileSizeLimitBytes: 10485760,
+                          rollOnFileSizeLimit: true,
+                          formatter: new Serilog.Formatting.Json.JsonFormatter(),
+                          restrictedToMinimumLevel: LogEventLevel.Information
+                      ));
+            });
+
+            return app;
         }
     }
 }
